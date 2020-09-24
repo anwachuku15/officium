ab57fa27d39cf5bcf16dc3bc839a4e19bb1e74c5
anwachuku15 <anwachuku15@gmail.com>
2020-09-24 17:58:14 -0400

    added description component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "./Home.css";
-import Carousel from "../components/Carousel";
-import Carousel1 from "../components/Carousel1";
+import Carousel from "../components/Carousel1";
+import Description from "../components/Description1";
 
 const Home = () => {
   return (
     <div className="home" onScroll={(e) => console.log(e.target.scrollTop)}>
-      <Carousel1 />
+      <Carousel />
+
+      {/* SERVICES COMPONENT */}
+      <Description />
+      {/* CLIENTS COMPONENT */}
+
+      {/* COMPANY MISSION */}
+
+      {/* TESTIMONIALS */}
+
+      {/* MESSAGE */}
     </div>
   );
 };
