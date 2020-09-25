0694bee855e36d941cee0db0b6c57fcb4d47439e
anwachuku15 <anwachuku15@gmail.com>
2020-09-24 21:28:09 -0400

    added Mission component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import "./Home.css";
 import Carousel from "../components/Carousel1";
 import Description from "../components/Description1";
 import Clients from "../components/Clients";
+import Mission from "../components/Mission";
+
 const Home = () => {
   return (
     <div className="home" onScroll={(e) => console.log(e.target.scrollTop)}>
@@ -13,8 +15,12 @@ const Home = () => {
       <Description />
       {/* CLIENTS COMPONENT */}
       <Clients />
-      {/* COMPANY MISSION */}
+      {/* DIVIDER */}
+      {/* <div className="divider__container">
 
+      </div> */}
+      {/* COMPANY MISSION */}
+      <Mission />
       {/* TESTIMONIALS */}
 
       {/* MESSAGE */}
