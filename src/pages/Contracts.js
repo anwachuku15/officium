3ecf3e7914b7675a134c7004d8f38cb2d3cb0455
anwachuku15 <anwachuku15@gmail.com>
2020-09-25 17:28:17 -0400

    added all pages components

---
+++ src/pages/Contracts.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Contracts.css";
+
+const Contracts = () => {
+  return <></>;
+};
+
+export default Contracts;
---
+++ src/pages/Sectors.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sectors.css";
+
+const Sectors = () => {
+  return <></>;
+};
+
+export default Sectors;
