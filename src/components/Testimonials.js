d907e059dccde9a02722c9e3aa8e18590322aa16
anwachuku15 <anwachuku15@gmail.com>
2020-09-25 14:08:59 -0400

    completed right half. TODO: animations

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Testimonials.css";
 
 const Testimonials = () => {
@@ -50,7 +51,28 @@ const Testimonials = () => {
           </div>
         </div>
 
-        <div className="testimonials__right"></div>
+        <div className="testimonials__right">
+          <div className="testimonials__rightTop">
+            <div className="testimonials__rightTopInner">
+              <div className="testimonials__rightTopInnerText">
+                <h4>JOIN US</h4>
+                <h2>
+                  We are selective about the clients we work with, but once a
+                  client we evolve with you all the way
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="testimonials__rightBottom">
+            <div className="testimonials__rightBottomInner">
+              <div className="testimonials__rightBottomButtonWrapper">
+                <Link to="/contact" className="testimonials__contactLink">
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
