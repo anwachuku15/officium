f3174d7a74c5148c7d20d568eb45c7e756df489b
anwachuku15 <anwachuku15@gmail.com>
2020-09-25 15:33:31 -0400

    completed home page. TODO: Animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import Description from "../components/Description1";
 import Clients from "../components/Clients";
 import Mission from "../components/Mission";
 import Testimonials from "../components/Testimonials";
-
+import ContactSection from "../components/ContactSection";
 const Home = () => {
   return (
     <div className="home" onScroll={(e) => console.log(e.target.scrollTop)}>
@@ -15,8 +15,7 @@ const Home = () => {
       <Clients />
       <Mission />
       <Testimonials />
-
-      {/* MESSAGE */}
+      <ContactSection />
     </div>
   );
 };
