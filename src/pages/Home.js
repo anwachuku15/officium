59039e1e9f6984491d63f89358d02fd5e4d65514
anwachuku15 <anwachuku15@gmail.com>
2020-09-25 14:09:14 -0400

    added Testimonials component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,23 +5,16 @@ import Carousel from "../components/Carousel1";
 import Description from "../components/Description1";
 import Clients from "../components/Clients";
 import Mission from "../components/Mission";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
     <div className="home" onScroll={(e) => console.log(e.target.scrollTop)}>
       <Carousel />
-
-      {/* SERVICES COMPONENT */}
       <Description />
-      {/* CLIENTS COMPONENT */}
       <Clients />
-      {/* DIVIDER */}
-      {/* <div className="divider__container">
-
-      </div> */}
-      {/* COMPANY MISSION */}
       <Mission />
-      {/* TESTIMONIALS */}
+      <Testimonials />
 
       {/* MESSAGE */}
     </div>
