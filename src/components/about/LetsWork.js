a6bab8d7b2a3f800126621810686605387af1f8f
anwachuku15 <anwachuku15@gmail.com>
2020-09-26 15:20:48 -0400

    completed LetsWork component

--- src/components/about/LetsWork.js
+++ src/components/about/LetsWork.js
@@ -1,8 +1,34 @@
+import { ArrowForwardIosRounded } from "@material-ui/icons";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LetsWork.css";
 
 const LetsWork = () => {
-  return <div className="letsWork__container"></div>;
+  return (
+    <div className="letsWork__container">
+      <div className="letsWork__contentContainer">
+        <div className="letsWork__contentHeaderContainer">
+          <div className="letsWork__contentHeaderDiv">
+            <div className="letsWork__contentHeaderText">
+              <h2>Let's Work Together</h2>
+            </div>
+          </div>
+        </div>
+        <div className="letsWork__contentButtonWrapper">
+          <div className="letsWork__contentButtonDiv">
+            <div className="getInTouchButton">
+              <Link to="/contact" className="getInTouchLink">
+                <span>GET IN TOUCH</span>
+              </Link>
+              <div className="getInTouchLink__arrow">
+                <ArrowForwardIosRounded fontSize="small" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LetsWork;
