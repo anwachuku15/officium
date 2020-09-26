05d4d1e5480b82cd25027129cb9eac5b2358769e
anwachuku15 <anwachuku15@gmail.com>
2020-09-26 11:35:33 -0400

    completed first section component for About page

--- src/components/about/AboutUs.js
+++ src/components/about/AboutUs.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./AboutUs.css";
 
 const AboutUs = () => {
-  return <div className="aboutUs__container"></div>;
+  return (
+    <div className="aboutUs__container">
+      <div className="aboutUs__contentContainer">
+        <div className="aboutUs__contentDiv">
+          <div className="aboutUs__content">
+            <div className="aboutUs__contentText">
+              <h1></h1>
+              <h1>About Us</h1>
+              <p>
+                NUES meaning Innovative Understanding and Evaluation of Science
+                represents our mission to you. We create innovative solutions to
+                your needs by evaluating the gaps through a different lens.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
